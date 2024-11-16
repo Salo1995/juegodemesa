bf0af347a04d084b46bd59dee09aeb159cbddf17
Salo1995 <102559544+Salo1995@users.noreply.github.com>
2024-11-16 02:49:10 -0300

    creacion de metodos crud

--- src/app/juegodemesa.service.spec.ts
+++ src/app/juegodemesa.service.spec.ts
@@ -1,13 +1,17 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing'; // Importa si tu servicio usa HttpClient
 
-import { juegodemesaService } from './juegodemesa.service';
+import { JuegodemesaService } from './juegodemesa.service';
 
-describe('AngularService', () => {
-  let service: juegodemesaService;
+describe('JuegodemesaService', () => {
+  let service: JuegodemesaService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(juegodemesaService);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule], // Importa el módulo de HttpClientTesting si es necesario
+      providers: [JuegodemesaService],
+    });
+    service = TestBed.inject(JuegodemesaService);
   });
 
   it('should be created', () => {
